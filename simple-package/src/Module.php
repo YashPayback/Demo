ed0dac61132ae916a45c16ed07d7573fcb38f8a5
YashPayback <yash.sarda@samarth.ac.in>
2024-04-10 16:33:43 +0530

    Second Commit

---
+++ simple-package/src/Module.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace demosaYashPayback\Module;
+/**
+ * demo module definition class
+ */
+
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'YashPayback\SimplePackage\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
