f0ac0ebca068d4aefa98a3593e9076670d5da29b
YashPayback <yash.sarda@samarth.ac.in>
2024-04-10 16:59:03 +0530

    changed file

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace demosaYashPayback\Module;
+namespace demo\SimplePackage\Module;
 /**
  * demo module definition class
  */
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * {@inheritdoc}
      */
-    public $controllerNamespace = 'YashPayback\SimplePackage\controllers';
+    public $controllerNamespace = 'demo\SimplePackage\controllers';
 
     /**
      * {@inheritdoc}
